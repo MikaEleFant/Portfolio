4fcf6ec32814d79136cc9f8d6f99e364c79192b7
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2021-03-03 15:43:55 -0800

    Comment out background change until dark mode implemented

--- script/background.js
+++ script/background.js
@@ -6,26 +6,26 @@ document.addEventListener("DOMContentLoaded", function(){
   let currentTimeHour = currentTimeArray[0].split(":")[0];
   let docHTML = document.documentElement;
 
-  if (currentTimeSuffix == "AM") {
-    if (parseInt(currentTimeHour) < 5) {
-      docHTML.style.backgroundImage = 'url("/resources/backgrounds/Mountains3.png")';
-    }
-    else if (parseInt(currentTimeHour) < 7) {
-      docHTML.style.backgroundImage = 'url("/resources/backgrounds/Mountains2.png")';
-    }
-    else {
-      docHTML.style.backgroundImage = 'url("/resources/backgrounds/Mountains1.png")';
-    }
-  }
-  else {
-    if (parseInt(currentTimeHour) > 5) {
-      docHTML.style.backgroundImage = 'url("/resources/backgrounds/Mountains2.png")';
-    }
-    else if (parseInt(currentTimeHour) > 7) {
-      docHTML.style.backgroundImage = 'url("/resources/backgrounds/Mountains3.png")';
-    }
-    else {
-      docHTML.style.backgroundImage = 'url("/resources/backgrounds/Mountains1.png")';
-    }
-  }
-});
+//   if (currentTimeSuffix == "AM") {
+//     if (parseInt(currentTimeHour) < 5) {
+//       docHTML.style.backgroundImage = 'url("/resources/backgrounds/Mountains3.png")';
+//     }
+//     else if (parseInt(currentTimeHour) < 7) {
+//       docHTML.style.backgroundImage = 'url("/resources/backgrounds/Mountains2.png")';
+//     }
+//     else {
+//       docHTML.style.backgroundImage = 'url("/resources/backgrounds/Mountains1.png")';
+//     }
+//   }
+//   else {
+//     if (parseInt(currentTimeHour) > 5) {
+//       docHTML.style.backgroundImage = 'url("/resources/backgrounds/Mountains2.png")';
+//     }
+//     else if (parseInt(currentTimeHour) > 7) {
+//       docHTML.style.backgroundImage = 'url("/resources/backgrounds/Mountains3.png")';
+//     }
+//     else {
+//       docHTML.style.backgroundImage = 'url("/resources/backgrounds/Mountains1.png")';
+//     }
+//   }
+// });
