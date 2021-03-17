08768c58f43bcbcfd58b77ec19466f4a21a6dfbb
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2021-03-17 12:57:07 -0700

    Revert commits to 'Implement star feature'

--- script/theme.js
+++ script/theme.js
@@ -1,4 +1,4 @@
-let currentTheme = localStorage.getItem("theme") ?? "light";
+let currentTheme = localStorage.getItem("theme") ?? "dark";
 
 document.onreadystatechange = function checkTheme() {
   if (currentTheme == "light") {
@@ -11,29 +11,24 @@ document.onreadystatechange = function checkTheme() {
 }
 
 function switchTheme() {
-  let root = document.documentElement;
   let themeCheck = document.getElementById("checkbox");
   let containerElements = document.querySelectorAll("h1, h2, h3, h6, p, a, li, span.theme-icon, #header-nav-close");
   let dividers = document.querySelectorAll("div.separator01, div.separator02");
   let containers = document.querySelectorAll("div.modal-main, nav.header-nav, #header-nav-close");
 
   if (themeCheck.checked) {
-    root.style.backgroundImage = "url('../resources/backgrounds/StarlitSky.png')";
+    document.documentElement.style.backgroundImage = "url('../resources/backgrounds/StarlitSky.png')";
     containerElements.forEach(containerElement => containerElement.style.color = "white");
     dividers.forEach(divider => divider.className = "separator01");
     containers.forEach(container => container.style.backgroundColor = "black");
-    root.style.setProperty("--highlight1", "rgba(255, 255, 255, 0.4)");
-    root.style.setProperty("--highlight2", "rgba(255, 255, 255, 0.3)");
 
     localStorage.setItem("theme", "dark");
   }
   else {
-    root.style.backgroundImage = "url('../resources/backgrounds/BlueSky.png')";
+    document.documentElement.style.backgroundImage = "url('../resources/backgrounds/BlueSky.png')";
     containerElements.forEach(containerElement => containerElement.style.color = "black");
     dividers.forEach(divider => divider.className = "separator02");
     containers.forEach(container => container.style.backgroundColor = "white");
-    root.style.setProperty("--highlight1", "rgba(0, 0, 0, 0.4)");
-    root.style.setProperty("--highlight2", "rgba(0, 0, 0, 0.3)");
 
     localStorage.setItem("theme", "light");
   }    
@@ -41,9 +36,7 @@ function switchTheme() {
 
 function revealThemeToggle() {
   let themeToggle = document.querySelector("div.theme-hider");
-  let themeWrapper = document.querySelector("div.theme-switch-wrapper");
 
   themeToggle.style.display = "flex";
-  themeWrapper.classList.remove("hidden");
   foundStar(1);
 }
